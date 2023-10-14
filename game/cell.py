25cff8eb535c7d46963e945b18f0503bd0681062
Aleric Cusher <alericcusher25@gmail.com>
2023-10-14 16:05:09 +0530

    Adds a Cell class to handle cell operations

--- game/cell.py
+++ game/cell.py
@@ -1,5 +1,13 @@
-from enum import Enum
+from typing import Tuple
+from game.cell_states import CellStatus
+from game.rules import Rules, StandardRules
 
-class Cell(Enum):
-    DEAD = 0
-    ALIVE = 1
+
+class Cell:
+    def __init__(self, location: Tuple[int, int], status: CellStatus = CellStatus.DEAD, rules: Rules = StandardRules):
+        self._location = location
+        self._status = status
+        self.rules = rules
+    
+    def get_state(self):
+        return self._status
--- tests/test_cell.py
+++ tests/test_cell.py
@@ -1,5 +1,19 @@
 from game.cell import Cell
+from game.cell_states import CellStatus
+from game.rules import StandardRules
 
-def test_cell():
-    assert Cell.ALIVE in Cell
-    assert Cell.DEAD in Cell
+def test_cell_creation():
+    location = (3, 3)
+    cell = Cell(location)
+    assert cell._location == location
+    assert cell._status == CellStatus.DEAD
+    assert cell.rules == StandardRules
+
+def test_get_status():
+    location = (3, 3)
+    cell = Cell(location)
+    assert cell.get_state() == CellStatus.DEAD
+
+    cell = Cell(location, status=CellStatus.ALIVE)
+    assert cell.get_state() == CellStatus.ALIVE
+    

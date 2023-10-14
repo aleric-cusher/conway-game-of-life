f9520829d43e63313dad421c96dec1b44d65c5fb
Aleric Cusher <alericcusher25@gmail.com>
2023-10-14 19:44:07 +0530

    Adds a step method to Grid to step through grid states

--- game/grid.py
+++ game/grid.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple
+from copy import copy
+from typing import List, Tuple, Union
 import random
 from game.cell import Cell
 from game.cell_states import CellStatus
@@ -6,7 +7,7 @@ from game.cell_states import CellStatus
 
 class Grid:
     def __init__(self, size: Tuple[int, int] = (30, 30), random_seed: bool = False, live_cell_locations: List[Tuple[int, int]] = None) -> None:
-        self._grid: List[Cell] = []
+        self._grid: List[List[Cell]] = []
         for i in range(size[0]):
             row = []
             for j in range(size[1]):
@@ -16,4 +17,22 @@ class Grid:
                     cell = Cell((i, j), status=random.choice(list(CellStatus)) if random_seed else CellStatus.DEAD)
                 row.append(cell)
             self._grid.append(row)
+
+        self.size = size
     
+    def get_grid(self, primitive_array: bool = False) -> Union[List[List[Cell]], List[List[int]]]:
+        if not primitive_array:
+            return copy(self._grid)
+        return [[cell.get_state().value for cell in row] for row in self._grid]
+        
+    def step(self) -> List[List[CellStatus]]:
+        grid = self.get_grid()
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                self._grid[i][j].evaluate_life(grid)
+        for i in range(self.size[0]):
+            for j in range(self.size[1]):
+                self._grid[i][j].update_status()
+
+        return self.get_grid()
+        
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -28,3 +28,24 @@ def test_grid_creation_with_alive_cells():
     grid._grid[2][2].get_state() == CellStatus.ALIVE
     grid._grid[3][4].get_state() == CellStatus.ALIVE
 
+def test_get_grid():
+    size = (5, 5)
+    grid = Grid(size, live_cell_locations=[(2, 2), (3, 4)])
+    gotten_grid = grid.get_grid()
+    assert gotten_grid == grid._grid
+
+def test_get_grid():
+    size = (2, 2)
+    grid = Grid(size, live_cell_locations=[(1, 1)])
+    expected_grid = [[0, 0], [0, 1]]
+    gotten_grid = grid.get_grid(primitive_array=True)
+    assert gotten_grid == expected_grid
+
+def test_step():
+    size = (5, 5)
+    grid = Grid(size, live_cell_locations=[(1, 1)])
+    grid.step()
+    grid_array = grid.get_grid()
+    for i in range(size[0]):
+        for j in range(size[1]):
+            assert grid_array[i][j].get_state() == CellStatus.DEAD

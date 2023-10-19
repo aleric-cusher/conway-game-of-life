a57aab927c25d97d561fa535d8df1ef17e770cc7
Aleric Cusher <alericcusher25@gmail.com>
2023-10-19 10:22:10 +0530

    Adds a get neighbours method to location

--- game/location.py
+++ game/location.py
@@ -1,11 +1,23 @@
 from __future__ import annotations
-from typing import List, Tuple
+from typing import List, Tuple, TYPE_CHECKING
+if TYPE_CHECKING:
+    from game.cell import Cell
 
 
 class Location:
+    neighbour_offsets = [(row, col) for row in [-1, 0, 1] for col in [-1, 0, 1] if not (row == 0 and col == 0)]
+
     def __init__(self, row: int, col: int) -> None:
         self.row = row
         self.col = col
+
+    def get_neighbours(self, grid: List[List[Cell]]) -> List[Cell]:
+        neighbours = []
+        for i, j in self.neighbour_offsets:
+            neighbour_coordinates = self + (i, j)
+            if neighbour_coordinates.is_valid(grid):
+                neighbours.append(grid[neighbour_coordinates.row][neighbour_coordinates.col])
+        return neighbours
 
     def is_valid(self, grid: List[List]) -> bool:
         return 0 <= self.row < len(grid) and 0 <= self.col < len(grid[0])
--- tests/test_location.py
+++ tests/test_location.py
@@ -1,4 +1,5 @@
 import pytest
+from game.grid import Grid
 from game.location import Location
 
 def test_location_creation():
@@ -13,6 +14,15 @@ def test_is_valid():
 
     location = Location(1, 0)
     assert location.is_valid(grid) == True
+
+def test_get_neighbours():
+    location = Location(0, 0)
+    grid = Grid((3, 3))
+    neighbours = location.get_neighbours(grid.get_grid_snapshot())
+    neighbour_locations = [Location(i, j) for j in range(2) for i in range(2)]
+    for neighbour in neighbours:
+        location = neighbour._location
+        assert location in neighbour_locations
 
 def test_add_coordinates():
     location = Location(3, 4)
